96eb7f59109eeeed2ed06be0c4cd65291a832a4d
adithya-naik <jatothadithyanaik@gmail.com>
2024-06-09 12:38:25 +0530

    ADDDED image and input time

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ function add(){
     let dateElement = document.querySelector('.todo-date');
     let inputDate = dateElement.value; 
 
-    todoList.push({item : inputText , dueDate : inputDate });
+    let timeElement = document.querySelector('.todo-time');
+    let inputTime = timeElement.value; 
+
+    let paraElement = document.querySelector('span');
+
+    todoList.push({item : inputText , dueDate : inputDate , dueTime : inputTime});
 
     inputElement.value= '';
     dateElement.value = "";
+    timeElement.value = "";
+    paraElement.innerText = '' ;
     
     display()
 }
@@ -19,10 +26,11 @@ function display(){
     let containerElement = document.querySelector('.show-container');
     let newHTMl = '';
     for(let i =0 ;i<todoList.length;i++){
-        let {item,dueDate} = todoList[i];
+        let {item,dueDate,dueTime} = todoList[i];
         newHTMl+=  `
         <span>${item}</span>
         <span>${dueDate}</span>
+        <span>${dueTime}</span>
         <button class = "btn-del btn" onclick = "todoList.splice(${i},1); display();">Delete</button>
         `
     }
